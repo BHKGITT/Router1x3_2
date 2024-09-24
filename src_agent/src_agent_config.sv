140d309c4357f882039e8c39eac43ad3c5db33d5
Hari Krishna <158833808+BHKGITT@users.noreply.github.com>
2024-09-24 21:35:17 +0530

    Create src_agent_config.sv

---
+++ src_agent/src_agent_config.sv
@@ -0,0 +1,15 @@
+class src_agent_config extends uvm_object;
+
+`uvm_object_utils(src_agent_config)
+
+
+uvm_active_passive_enum is_active=UVM_ACTIVE;
+
+
+virtual src_if vif;
+
+function new(string name="src_agent_config");
+super.new(name);
+endfunction
+
+endclass
